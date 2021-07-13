d72e60179cbd1ac36b9f1f2cf6d2a550aead2811
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-13 04:59:49 +0000

    Adding cde that prints in C#

---
+++ 0x00-csharp-hello_world/3-writeline.cs
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+using System;
+
+namespace Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programming is like building a multilingual puzzle);
+        }
+    }
+}
