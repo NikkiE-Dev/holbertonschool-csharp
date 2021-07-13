a7d2284db2a92f7e024e726ca4b21f32fd822933
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-13 23:50:34 +0000

    Print hexadecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a <= 98; a++)
+            {
+                Console.Write("{0} = 0x{0:X}\n", a);
+            }
+        }
+    }
+}
