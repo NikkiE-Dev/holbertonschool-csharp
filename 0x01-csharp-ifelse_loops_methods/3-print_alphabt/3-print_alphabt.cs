9d83e541f348da666242cf0d23b6d51cce768d93
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-13 23:17:49 +0000

    Code for abc skip e & q

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char alphabet = 'a'; alphabet <= 'z'; alphabet++)
+            {
+                if (alphabet != 'e' && alphabet != 'q')
+                    Console.Write("{0}", alphabet);
+            }
+        }
+    }
+}
