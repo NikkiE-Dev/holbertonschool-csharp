485097ebf9675157f432e10c516674a8c42a678d
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-14 00:44:39 +0000

    Code for number 2 decimal format

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 99; a++)
+            {
+                if (a <= 98)
+                {
+                    Console.Write("{0:D2}, ", a);
+                }
+                else
+                {
+                    Console.Write("{0:D2}\n", a);
+                }
+                
+            } 
+        }
+    }
+}
