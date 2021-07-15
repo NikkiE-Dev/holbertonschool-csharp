4143c36ed6c67141ec419e010517c772bf1461e7
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-15 03:03:49 +0000

    Adding code to print array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _0_print_array
+{
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size == 0)
+            {
+                Console.WriteLine("");
+            }
+            else if (size < 0)
+            {
+               Console.WriteLine("Size cannot be negative");
+               return null;
+            }
+            else
+            {
+                int[] array = new int[size]
+                for (int a = 0; a < size; a++)
+                {
+                    array[a] = a;
+                    if (a < size)
+                        Console.Write(" ");
+                    Console.Write(a);
+                }
+                Console.WriteLine("");
+                return array;
+                    }
+                }
+            }
+            
+        }
+    }
+}
