1ff7317cf0f6c113d3dc45274db68442945587d3
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-16 00:52:33 +0000

    Code to add integers in list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            int sum = 0;
+            List<int> sumList = myList;
+            for (int a = 0; a < sumList.Count; a++)
+            {
+                if (sumList[a] != sumList[a - 1] || a == 0)
+                    sum += sumList[a];
+            }
+            return (sum);
+        }
+    }
