7ae8b0154b94508f35f21148fab3e7617af71e9c
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-18 22:52:54 +0000

    Code that prints all possible diff 2 digit combos

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            for (a = 0; a < 10; a++)
+            {
+                for (int b = a + 1; b < 10; b++)
+                {
+                    if (a == 8 && b == 9)
+                    {
+                        Console.WriteLine("{0}{1}", a, b);
+                    }
+                    else
+                    {
+                        Console.Write("{0}{1}, ", a, b);
+                    }
+                } 
+            } 
+        }
+    }
+}
