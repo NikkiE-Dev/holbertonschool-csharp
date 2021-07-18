6ff34879c05b6705967cc07e36e3f558bc521f5e
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-18 00:04:47 +0000

    Code to replace %3 & %5 with words

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            for (a = 1; a <= 100; a++)
+            {
+                if (a % 5 == 0 && a % 3 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (a % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (a % 5 == 0)
+                {
+                    if (a == 100)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    Console.Write("Buzz ");
+                }
+                else
+                {
+                    Console.Write("{0} ", a);
+                }
+            } 
+        }
+    }
+}
