3e2ffe52b565d31b07f264b3d12fb56e6daee665
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-18 02:09:30 +0000

    Code to print last digit

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            int absvalue = Math.Abs(number);
+            int lstdg = absvalue % 10;
+            
+            Console.Write(lstdg);
+            return (lstdg);
+        }
+    }
