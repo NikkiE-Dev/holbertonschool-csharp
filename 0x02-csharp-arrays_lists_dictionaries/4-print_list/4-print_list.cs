2a6c7c4dd3b6d27ddc10f9ee58469b6bb20cd6e9
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-19 02:29:18 +0000

    Code to pritn number list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.Write("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            List<int> printlst = new List<int> ();
+            for (int a = 0; a < size; a++)
+            {
+                printlst.Add(a);
+                if (a == 0)
+                {
+                    Console.Write("{0}", a); 
+                }
+                else 
+                {
+                    Console.Write(" {0}", a);
+                }
+            }
+            Console.WriteLine("");
+            return printlst;
+        }
+    }
+}
