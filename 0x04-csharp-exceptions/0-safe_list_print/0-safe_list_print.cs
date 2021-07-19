0fe2d94395f74d9923c9d7c463eda292465cf00a
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-19 20:37:20 +0000

    Code to print list

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int a = 0;
+        try
+        {
+            for (; a < n; a++)
+                Console.WriteLine("{0}", myList[a]);
+        }
+        catch (Exception)
+        {
+            return (a);
+        }
+        return (a);
+    }
+}
