695600e119b1316ee0465fb2b444e4b33b24ad9a
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-20 20:47:23 +0000

    Code to add .ToString method

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,24 @@
+﻿public struct Dog
+{
+    public string name {get; set;}
+    public float age {get; set;}
+    public string owner {get; set;}
+    public Rating rating {get; set;}
+    public Dog(string name, float age, string owner, Rating rating): this()
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+    public override string ToString()
+    {
+      return string.Format("Dog Name: {0}\nAge: {1}\nOwner: {2}\nRating: {3}\n", name, age, owner, rating);
+    }
+}
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
