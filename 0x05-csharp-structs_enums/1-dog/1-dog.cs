dd9d0c52fcba803cacb68669d11ba71e09ed006a
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-20 19:01:20 +0000

    Code to add strcut to enum

---
+++ 0x05-csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,13 @@
+﻿public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
