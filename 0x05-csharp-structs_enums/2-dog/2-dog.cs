6129650a4af58e3643f923dc1291e5857f13f675
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-20 19:53:15 +0000

    Code to add constuctor with parameters

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,23 @@
+﻿public struct Dog
+{
+    public Dog(string name, float age, string owner, Rating rating): this()
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+
+    }
+    public string name {get; set;}
+    public float age {get; set;}
+    public string owner {get; set;}
+    public Rating rating {get; set;}
+        
+
+}
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
