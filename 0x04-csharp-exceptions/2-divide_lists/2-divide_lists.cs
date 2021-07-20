6086474594331b8ed0dbccef61894aee47d46220
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-20 02:38:37 +0000

    Code to divide 2 lists

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+    
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> result = new List<int>();
+        int b = 0;
+
+            for (int a = 0; a < listLength; a++)
+            {
+                try
+                {
+                    b = list1[a] / list2[a];
+                    result.Add(b);
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                    result.Add(0);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Out of range");
+                }
+                
+            }
+        return (result);
+    }
+}
