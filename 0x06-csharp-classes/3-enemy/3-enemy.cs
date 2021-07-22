fe793f01314bd39cf9ac27ec95625d28094055cb
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-22 02:32:34 +0000

    Code to add public method

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Creates a Zombie.</summary>
+    public class Zombie
+    {
+        ///<summary>Creates a public field.</summary>
+        public int health;
+            ///<summary>Creates a public constructor.</summary>
+            public Zombie()
+            {
+                health = 0;
+            }
+            ///<summary>Creates a 2nd public constructor.</summary>
+            public Zombie(int value)
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Health must be greater than or equal to 0");
+                }
+                else
+                {
+                    health = value;
+                }
+                
+            }
+            ///<summary>Creates a public method.</summary>
+            public int GetHealth()
+            {
+                return health;
+            }
+            
+    }
+       
+}
