fb2f4348fd27f580bd7462a6eca8554e14ca0eeb
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-22 02:00:24 +0000

    Code to add 2nd constructor

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Creates a Zombie.</summary>
+    public class Zombie
+    {
+        ///<summary>Creates a public field.</summary>
+        public int health;
+        ///<summary>Creates a public constructor.</summary>
+            public Zombie()
+            {
+                this.health = 0;
+            }
+            ///<summary>Creates a 2nd public constructor.</summary>
+            public Zombie(int value)
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Health must be greater than or equal to 0");
+                }
+            }
+    }
+       
+}
