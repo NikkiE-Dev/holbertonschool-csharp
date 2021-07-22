05fa596704ccc3eb30fed3ce56c3d726b39db231
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-22 02:06:35 +0000

    Corrected code to assign value to health

--- 0x06-csharp-classes/2-enemy/2-enemy.cs
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -7,10 +7,10 @@ namespace Enemies
     {
         ///<summary>Creates a public field.</summary>
         public int health;
-        ///<summary>Creates a public constructor.</summary>
+            ///<summary>Creates a public constructor.</summary>
             public Zombie()
             {
-                this.health = 0;
+                health = 0;
             }
             ///<summary>Creates a 2nd public constructor.</summary>
             public Zombie(int value)
@@ -18,6 +18,10 @@ namespace Enemies
                 if (value < 0)
                 {
                     throw new ArgumentException("Health must be greater than or equal to 0");
+                }
+                else
+                {
+                    health = value;
                 }
             }
     }
