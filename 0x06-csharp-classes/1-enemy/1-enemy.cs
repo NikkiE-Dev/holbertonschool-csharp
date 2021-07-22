97d5fe93d49cca312ee9471cd76a5b82aef4d659
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-22 00:44:29 +0000

    Code that adds public field & public constructor

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Creates a Zombie.</summary>
+    public class Zombie
+    {
+        ///<summary>Creates a public field.</summary>
+        public int health;
+        ///<summary>Creates a public constructor.</summary>
+            public Zombie()
+            {
+                this.health = 0;
+            }
+    }
+       
+}
