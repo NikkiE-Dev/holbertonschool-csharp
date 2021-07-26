380399f2c8e9153aa1287e5bfafebb8f9ab941f0
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-26 22:53:56 +0000

    Adding code & tests for addition

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Positive_Integers()
+        {
+            int a = 50;
+            int b = 75;
+            int c = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, c);
+        }
+        [Test]
+        public void Negative_Integers()
+        {
+            int a = -50;
+            int b = -25;
+            int c = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, c);
+        }
+        [Test]
+        public void Negative_Positive_Integers()
+        {
+            int a = 100;
+            int b = -20;
+            int c = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, c);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyMath
+{
+    ///<summary>Creates a public class.</summary>
+    public class Operations
+    {
+        ///<summary>Creates a protype.</summary>
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
