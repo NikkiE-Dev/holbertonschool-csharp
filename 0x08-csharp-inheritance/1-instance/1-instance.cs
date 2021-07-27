b974f44d2c95c1455176d3b9c76c7255499cea14
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 18:37:45 +0000

    Code to check if obj is inst or inherited of class

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+///<summary>Creates a class.</summary>
+class Obj
+{
+    ///<summary>Creates a prototype.</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (typeof(Array).IsInstanceOfType(obj))
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+}
