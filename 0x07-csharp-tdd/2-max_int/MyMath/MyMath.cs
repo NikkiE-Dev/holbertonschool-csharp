5d40ab045371c76bf1e6d14c2285e1cc2c11f5f2
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 02:39:41 +0000

    Updated documentation

--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -3,14 +3,14 @@ using System.Linq;
 
 namespace MyMath
 {
-    ///<summary>Creates a public class.</summary>
+    ///<summary>Creates a public class named Operations.</summary>
     public class Operations
     {
-        ///<summary>Creates a protype.</summary>
+        ///<summary>Creates a protype that finds Max Integer.</summary>
         public static int Max(List<int> nums)
         {
             int result = 0;
-            if (nums.Count == 0 || nums == null)
+            if (nums.Count == 0)
             {
                 return 0;
             }
