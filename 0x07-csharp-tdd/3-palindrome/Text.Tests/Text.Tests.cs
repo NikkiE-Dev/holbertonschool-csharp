ac1c14440126c8a0a7c2325916e480f8b480cf48
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 04:48:00 +0000

    Updating code

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -10,9 +10,29 @@ namespace Text.Tests
     public class Tests
     {
         [Test]
-        public void Palidrome()
+        public void Palindrome()
         {
             Assert.True(Text.Str.IsPalindrome("doggod"));
+        }
+         [Test]
+        public void Palindrome_Upper()
+        {
+            Assert.True(Text.Str.IsPalindrome("Doggod"));
+        }
+         [Test]
+        public void Palindrome_Punc()
+        {
+            Assert.True(Text.Str.IsPalindrome("dog:god"));
+        }
+         [Test]
+        public void Palindrome_Space()
+        {
+            Assert.True(Text.Str.IsPalindrome("dog god"));
+        }
+         [Test]
+        public void Palidrome_Empty()
+        {
+            Assert.True(Text.Str.IsPalindrome(""));
         }
 
     }
