0bfab6660a194fbabe432adba2d27001084e2be8
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 18:04:19 +0000

    Adding code to check if obj is int

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj is int)
+            return true;
+
+        else
+        {
+            return false;
+        }
+    }
+}
