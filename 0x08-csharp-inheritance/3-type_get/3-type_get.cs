7825b8646150a6abc322d77cb58cb9a53621e322
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 20:21:40 +0000

    Code to get properties & methods of var

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+///<summary>Creates a class.</summary>
+class Obj
+{
+    ///<summary>Creates a prototype.</summary>
+    public static void Print(object myObj)
+    {
+        TypeInfo l = myObj.GetType().GetTypeInfo();
+        Console.WriteLine("{0} Properties:", l.Name);
+        foreach (PropertyInfo item in l.GetProperties())
+        {
+            Console.WriteLine(item.Name);
+        }
+        Console.WriteLine("{0} Methods:", l.Name);
+        foreach (MethodInfo thing in l.GetMethods())
+        {
+            Console.WriteLine(thing.Name);
+        }
+    }
+}
