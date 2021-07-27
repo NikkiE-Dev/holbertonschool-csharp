52c0b9264802f78c11477d1920bb264ea9b0ff8a
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 04:12:51 +0000

    Updating test case

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -1,12 +1,20 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace Text.Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Palidrome()
         {
+            string text = "level";
+            string textrev = Text.Str.IsPalindrome(text);
+            Assert.IsTrue(text == textrev);
+
         }
 
         [Test]
