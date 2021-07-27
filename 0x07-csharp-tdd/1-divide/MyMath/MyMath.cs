3e82646929dd9538cb232853ca8c909281b292c4
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 00:43:49 +0000

    Adding code for dividing matrix

---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyMath
+{
+    ///<summary>Creates a public class.</summary>
+    public class Matrix
+    {
+        ///<summary>Creates a protype.</summary>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (matrix == null)
+            {
+                return null;
+            }
+            if (num == 0)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            int mrow = matrix.GetLength(0);
+            int mcolumn = matrix.GetLength(1);
+            int[,] newMatrix = new int[mrow, mcolumn];
+            for (int a = 0; a < mrows; a++)
+            {
+                for (int b = 0; b < mcolumn; b++)
+                {
+                    newMatrix[a, b] = matrix[a,b] / num;
+                }
+            }
+            return (newMatrix);
+        }
+    }
+}
