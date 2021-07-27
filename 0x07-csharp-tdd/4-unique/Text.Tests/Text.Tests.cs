53df9c74d3a340748abaa52bfd700460e2e89dfd
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 05:18:23 +0000

    Adding first test case

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -4,9 +4,12 @@ namespace Text.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Non_rep_first()
         {
+            string test = "Howdyyall";
+            int result = Text.Str.UniqueChar(test);
+            Assert.IsEqual(result, 0);
         }
 
         [Test]
