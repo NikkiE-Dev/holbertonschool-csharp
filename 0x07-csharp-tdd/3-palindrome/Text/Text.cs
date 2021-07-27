de4f6f9e566179ddec9937f7fcea5bd489bce70e
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 04:03:30 +0000

    Adding code for palidrome

---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Text
+{
+    public class Str
+    {
+        public static bool IsPalindrome(string s)
+        {
+            List<char> basic = new List<char>();
+            foreach (char item in s)
+            {
+                if (!Char.IsPunctuation(item) && !Char.IsWhiteSpace(item))
+                {
+                    basic.Add(Char.ToLower(item));
+                }
+            }
+            if (basic.Count == 0)
+                return (true);
+            List<char> backwards = new List<char>(basic);
+            backwards.Reverse();
+            return (basic.SequenceEqual(backwards));
+        }
+    }
+}
