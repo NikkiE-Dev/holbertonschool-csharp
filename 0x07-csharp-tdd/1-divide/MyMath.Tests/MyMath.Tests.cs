d17510237025ab11c1a8d8fdb57ab0ea2f9e7d89
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 01:08:49 +0000

    Adding Test case

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,34 @@
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace MyMath.Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Regular_Division()
+        {
+            int[,] matrix = new int[,]
+            {
+                {50, 25, -20},
+                {40, 548, 120},
+                {88, 42, -10}
+            };
+            int[,] result;
+            result = MyMath.Matrix.Divide(matrix, 2);
+            Assert.That(Is.EqualTo(output), result);
+        }
+
+        [Test]
+        public void Division_By_Zero()
         {
         }
 
         [Test]
-        public void Test1()
+        public void Null_Matrix()
         {
-            Assert.Pass();
         }
     }
 }
