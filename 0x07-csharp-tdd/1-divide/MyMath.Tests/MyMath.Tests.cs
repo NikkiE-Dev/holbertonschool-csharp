f457e5a7c5c0c665683be77f3437e11276febe61
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 01:37:32 +0000

    Updating test case

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -10,27 +10,19 @@ namespace MyMath.Tests
         [Test]
         public void Regular_Division()
         {
-            int[,] matrix = new int[,]
-            {
-                {50, 25, -20},
+            int[,] matrix = {
+                {50, 20, -20},
                 {40, 548, 120},
                 {88, 42, -10}
             };
-            int[,] result;
-            int i, j;
-            result = MyMath.Matrix.Divide(matrix, 2);
-            if (result != null)
-            {
-                for (i = 0; i < result.GetLength(0); i++)
-                {
-                    for (j = 0; j < result.GetLength(1); j++)
-                    {
-                        Console.Write(result[i,j] + " ");
-                    }
-                    Console.WriteLine();
-                }
-            }
-            Assert.AreEqual(result, result);
+            int num = 2;
+            int[,] newMatrix = {
+                {25, 10, -10},
+                {20, 274, 60},
+                {44, 21, -5}
+            };
+            int[,] result = MyMath.Matrix.Divide(matrix, 2);
+            Assert.AreEqual(result, newMatrix);
         }
 
         [Test]
