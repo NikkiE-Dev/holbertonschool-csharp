ea856332379ac1104a457a8ced2f8f3fd2525410
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 05:08:46 +0000

    Adding code for unique character

---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Text
+{
+    ///<summary>Creates a public class.</summary>
+    public class Str
+    {
+        ///<summary>Creates a protype.</summary>
+        public static int UniqueChar(string s)
+        {
+            List<char> basic = new List<char>();
+            Dictionary<char, int> grab = new Dictionary<char, int>();
+            int a = -1;
+            for (int b = 0; b < s.Length; b++)
+            {
+                if (basic.Contains(s[b]))
+                    continue;
+                if (grab.ContainsKey(s[b]))
+                {
+                    basic.Add(s[b]);
+                    grab.Remove(s[b]);
+                    continue;
+                }
+                grab.Add(s[b], b);
+            }
+            foreach (KeyValuePair<char, int> item in grab)
+            {
+                if (item.Value < a || a == -1)
+                    a = item.Value;
+            }
+            return(a);
+        }
+    }
+}
