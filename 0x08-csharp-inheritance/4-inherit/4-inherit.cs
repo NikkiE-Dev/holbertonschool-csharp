6b83570a153c388ee5cf5ba78742df22d75b1ace
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 20:31:22 +0000

    Code to create inherted class

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Creates an empty class.</summary>
+class Animal
+{
+}
+
+///<summary>Creates an empty class that inherits from empty class.</summary>
+class Dog : Animal
+{
+}
