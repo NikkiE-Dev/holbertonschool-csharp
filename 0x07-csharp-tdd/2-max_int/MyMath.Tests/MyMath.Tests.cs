9ee64be7492343be257d19c391c85844c1188be1
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-27 02:56:15 +0000

    Updating documentation in test file

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 
 namespace MyMath.Tests
 {
+    /// <summary>Test case for Max int.</summary>
     [TestFixture]
     public class Tests
     {
