a303e947841f33cb4946f066c5a45f03fab0aeef
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-30 16:15:19 +0000

    Adding additional test cases

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -4,20 +4,43 @@ using NUnit.Framework;
 
 namespace Text.Tests
 {
+    [TestFixture]
     public class Tests
     {
         [Test]
         public void Non_rep_first()
         {
-            string test = "Howdyyall";
+            string test = "Hoowwdyyaall";
             int result = Text.Str.UniqueChar(test);
-            Assert.Equals(result, 0);
+            Assert.AreEqual(result, 0);
         }
-
         [Test]
-        public void Test1()
+        public void Non_rep_last()
         {
-            Assert.Pass();
+            string test = "HHoowwddyyaal";
+            int result = Text.Str.UniqueChar(test);
+            Assert.AreEqual(result, 12);
+        }
+        [Test]
+        public void Non_rep_mid()
+        {
+            string test = "HHoowwdyyaall";
+            int result = Text.Str.UniqueChar(test);
+            Assert.AreEqual(result, 6);
+        }
+        [Test]
+        public void No_rep_char()
+        {
+            string test = "Howdy";
+            int result = Text.Str.UniqueChar(test);
+            Assert.AreEqual(result, 0);
+        }
+        [Test]
+        public void Empty_string()
+        {
+            string test = "";
+            int result = Text.Str.UniqueChar(test);
+            Assert.AreEqual(result, -1);
         }
     }
 }
