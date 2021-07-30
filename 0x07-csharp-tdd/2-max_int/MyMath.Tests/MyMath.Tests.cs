37899378661688ad83113dfc7a963cbdfcf3d9d9
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-30 14:19:46 +0000

    Updating test cases

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -7,15 +7,47 @@ namespace MyMath.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Max_Int_Middle()
         {
+            List<int> nums = new List<int>(){18, 24, 128, 2, 64};
+            int max = 128;
+            int result = MyMath.Operations.Max(nums);
+            Assert.That(result, IsEqualTo(128));
         }
 
         [Test]
-        public void Test1()
+        public void Empty_List()
         {
-            Assert.Pass();
+            List<int> nums = new List<int>();
+            int max = 0;
+            int result = MyMath.Operations.Max(nums);
+            Assert.IsNull(nums);
+        }
+
+        [Test]
+        public void Max_Int_Neg()
+        {
+            List<int> nums = new List<int>(){18, 24, -2, 64, 128};
+            int max = 128;
+            int result = MyMath.Operations.Max(nums);
+            Assert.That(result, IsEqualTo(128));
+        }
+        [Test]
+        public void Max_Int_Beginning()
+        {
+            List<int> nums = new List<int>(){128, 18, 24, 2, 64};
+            int max = 128;
+            int result = MyMath.Operations.Max(nums);
+            Assert.That(result, IsEqualTo(128));
+        }
+        [Test]
+        public void Max_Int_End()
+        {
+            List<int> nums = new List<int>(){18, 24, 2, 64, 128};
+            int max = 128;
+            int result = MyMath.Operations.Max(nums);
+            Assert.That(result, IsEqualTo(max));
         }
     }
 }
