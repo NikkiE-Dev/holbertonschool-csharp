fd3e4f8aeb031227777c87de617f0d9e67b23968
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-30 18:39:55 +0000

    Code to count words

---
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Text
+{
+    ///<summary>Creates a public class.</summary>
+    public class Str
+    {
+        ///<summary>Creates a protype.</summary>
+        public static int CamelCase(string s)
+        {
+            int count = 0, idx = 0;
+
+            while (index < s.Length && char.IsWhiteSpace(s[idx]))
+                idx++;
+
+            while (idx < s.Length)
+            {
+                while (idx < s.Length && !char.IsWhiteSpace(s[idx]))
+                    idx++;
+
+                count++;
+
+
+                while (idx < s.Length && char.IsWhiteSpace(s[idx]))
+                    idx++;
+            }
+        }
+    }
+}
