10aa5dc00e3a71c9f9c867df1888e0c20914eee8
NikkiE-Holberton <2495@holbertonschool.com>
2021-08-03 02:53:49 +0000

    Updating code

--- 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -9,10 +9,10 @@
             double[] result = new double[1] {-1};
             
             if (vector.Length == 2)
-                result = new double[2] {vector[0] * scalar, vector1[1] +  * scalar};
+                result = new double[2] {vector[0] * scalar, vector[1] * scalar};
 
             if (vector.Length == 3)
-                result = new double[3] {vector[0] * scalar, vector1[1] +  * scalar, vector1[2] * scalar};
+                result = new double[3] {vector[0] * scalar, vector[1] * scalar, vector[2] * scalar};
         
             return (result);
         }   
