751d847e169b1c3b70697512c72fb325609e15dd
NikkiE-Holberton <2495@holbertonschool.com>
2021-08-03 03:09:05 +0000

    Cdoe to calculate dot product of 2D or 3D vectors

---
+++ 0x00-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+    ///<summary>Creates a class to calculate vector.</summary>
+    class VectorMath
+    {
+        ///<summary>Creates a protype with parameters.</summary>
+        public static double DotProduct(double[] vector1, double[] vector2)
+        {
+            double[] result = new double[1] {-1};
+            
+            if (vector1.Length == 2 && vector2.Length == 2)
+                result = new double[1] {vector1[0] * vector2[0] + vector1[1] * vector2[1]};
+
+            if (vector1.Length == 3 && vector2.Length == 3)
+                result = new double[1] {vector1[0] * vector2[0] + vector1[1] * vector2[1] + vector1[2] * vector2[2]};
+        
+            return (result);
+        }   
+        
+    }
+
