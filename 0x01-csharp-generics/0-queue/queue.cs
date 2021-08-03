0d8abe5f5e284e0f279786e53c51d43ade695a8b
NikkiE-Holberton <2495@holbertonschool.com>
2021-08-03 20:02:37 +0000

    Code to create a new class & method

---
+++ 0x01-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>Creates a new class.</summary>
+public class Queue<T>
+{
+    ///<summary>Creates a method that returns the Queue’s type.</summary>
+    public string CheckType()
+    {
+        return (typeof(T).ToString());
+    }
+}
