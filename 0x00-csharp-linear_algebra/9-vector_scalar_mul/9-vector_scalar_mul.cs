fe0a136cc86facdc665b72699263ab583a6f60be
NikkiE-Holberton <2495@holbertonschool.com>
2021-08-03 02:48:27 +0000

    Code to multiply vector by scalar & returns result

---
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    ///<summary>Creates a class to calculate vector.</summary>
+    class VectorMath
+    {
+        ///<summary>Creates a protype with parameters.</summary>
+        public static double[] Multiply(double[] vector, double scalar)
+        {
+            double[] result = new double[1] {-1};
+            
+            if (vector.Length == 2)
+                result = new double[2] {vector[0] * scalar, vector1[1] +  * scalar};
+
+            if (vector.Length == 3)
+                result = new double[3] {vector[0] * scalar, vector1[1] +  * scalar, vector1[2] * scalar};
+        
+            return (result);
+        }   
+        
+    }
