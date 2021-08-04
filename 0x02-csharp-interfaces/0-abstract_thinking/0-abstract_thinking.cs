d4fd29182d46b2273ca55219522126f5cd3128d3
NikkiE-Holberton <2495@holbertonschool.com>
2021-08-04 21:47:34 +0000

    Code for abstract base and properties

---
+++ 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+///<summary>Creating an abstarct class.</summary>
+public abstract class Base
+{
+    ///<summary>Creates property obj named "name".</summary>
+    public string name;
+
+    ///<summary>Method that prints obj and obj type.</summary>
+    public override string ToString()
+    {
+        return (name + " is a " + this.GetType().ToString());
+    }
+}
