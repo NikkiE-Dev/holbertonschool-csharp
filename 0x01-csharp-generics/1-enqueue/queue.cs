b3f4aa285cc7fe61518915ae6277d445a1d1cf1f
NikkiE-Holberton <2495@holbertonschool.com>
2021-08-04 03:43:35 +0000

    Adding code

---
+++ 0x01-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+///<summary>Creates a new class.</summary>
+public class Queue<T>
+{
+    ///<summary>Creates properties Queue class.</summary>
+    public Node head = null;
+    public Node tail = null;
+    public int count = 0;
+    
+    ///<summary>Creates a method that returns the Queue’s type.</summary>
+    public string CheckType()
+    {
+        return (typeof(T).ToString());
+    }
+
+    ///<summary>Creates a method that creates a new Node and adds it to the end of the queue.</summary>
+    public void Enqueue(T value)
+    {
+        Node nxt = new Node(value);
+        if (head == null && tail == null)
+        {
+            head = nxt;
+            tail = nxt;
+        }
+        else
+        {
+            tail.next = nxt;
+            tail = nxt;
+        }
+        count += 1;
+    }
+    ///<summary>Creates a method that creates a that returns the number of nodes in the Queue.</summary>
+    public int Count()
+    {
+        return count;
+    }
+
+    ///<summary>Creates a 2nd new class.</summary>
+    public class Node
+    {
+        public T value = default(T);
+        public Node next = null;
+
+        ///<summary>Creates a constuctor for Node class.</summary>
+        public Node (T v)
+        {
+            value = v;
+        }
+    }
+}
