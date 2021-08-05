69e0b56252824f48d084b8530be29fb0ffe081b6
NikkiE-Holberton <2495@holbertonschool.com>
2021-08-05 19:44:56 +0000

    Code to add public class & private properties

---
+++ 0x03-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+///<summary>Creating an public class.</summary>
+public class Player
+{
+    ///<summary>Creating properties for class.</summary>
+    private string name { get; set; }
+
+    ///<summary>Creating properties for class.</summary>
+    private float maxHp { get; set; }
+   
+    ///<summary>Creating properties for class.</summary>
+    private float hp { get; set; }
+    
+    ///<summary>Creating prototype.</summary>
+    public Player(string name= "Player", float maxHp= 100f)
+    {
+        if (maxHp <= 0)
+        {
+            maxHp= 100f;
+            Console.WriteLine("maxHp must be greater than 0.");
+            Console.WriteLine("maxHp set to 100f by default.");
+        }
+
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+
+    }
+
+    ///<summary>Creating prototype.</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+}
+
